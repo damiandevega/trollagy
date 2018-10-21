31ef0a2894182355240a3cef08ba9bc76cf62606
Damian de Vega <damiandevega@gmail.com>
2018-10-21 15:54:49 -0700

    adding client and other directories

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://damian:damian6@ds155252.mlab.com:55252/trollagy'
+    mongoURI: 'mongodb://damian:damian6@ds155252.mlab.com:55252/trollagy',
+    secretOrKey: 'secret'
 };
